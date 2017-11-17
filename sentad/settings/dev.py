8c53c01668628dcdba9f50551559de8aaa68017d
Soufiane Mghanen <mgh.soufiane@gmail.com>
2017-11-17 23:15:24 +0000

    Implementing per-environment settings #14

--- sentad/settings/dev.py
+++ sentad/settings/dev.py
@@ -1,14 +1,16 @@
-from .common import *
+from sentad.settings.common import *
 import os
+
+
 ALLOWED_HOSTS = ['*']
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'USER': "ebroot",
+        'PASSWORD': "Fousiane0",
         'HOST': os.environ['RDS_HOSTNAME'],
         'PORT': os.environ['RDS_PORT'],
     }
--- sentad/settings/prod.py
+++ sentad/settings/prod.py
@@ -1,15 +1,16 @@
-from .common import *
+from sentad.settings.common import *
 import os
 
+
 ALLOWED_HOSTS = ['*']
-DEBUG = True
+DEBUG = False
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['RDS_DB_NAME'],
-        'USER': "ebroot",
-        'PASSWORD': "Fousiane0",
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
         'HOST': os.environ['RDS_HOSTNAME'],
         'PORT': os.environ['RDS_PORT'],
     }
