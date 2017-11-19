18764db77d142ced9fa1d2db5278441f92781d0b
Soufiane Mghanen <mgh.soufiane@gmail.com>
2017-11-19 14:38:27 +0000

    testing new graphql

--- sentad/schema.py
+++ sentad/schema.py
@@ -1,12 +1,19 @@
-from authentication.schema import accountQuery
+from authentication.schema import AccountQuery, PermissionQuery, GroupQuery, CreateGroup
 import graphene
 
 
+class Mutation(graphene.ObjectType):
+    create_group = CreateGroup.Field()
+
+
 class Query(
-    accountQuery,
+    PermissionQuery,
+    AccountQuery,
+    GroupQuery,
+    # Finally import graphene ObjectType
     graphene.ObjectType
 ):
     pass
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
